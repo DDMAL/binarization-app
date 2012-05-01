95de9670678d6ba0f2b97efb599084037a8cdd77
Saining Li <saining.li@gmail.com>
2012-05-01 14:08:00 -0400

    added comments to binarize.js

--- static/js/binarize.js
+++ static/js/binarize.js
@@ -2,14 +2,18 @@ $(document).ready (function() {
 
     var originalImageSrc = $("img").attr("src");
     $("#selections").change(function() {
+        // change sliders depending on option selected
         var value = $("#selections option:selected").val();
         var node = $("#sliders");
         node.hide();
         node.children().remove();
         if (value =="sel") {
+            // "Make a selection" option - show original image
             $("img").attr("src", originalImageSrc);
         } else if (value == "sauvola") {
+            // show default image
             $("img").attr("src", originalImageSrc);
+            // create sliders (hidden) with gamera default values
             var s1 = newSliderDiv("regionSize");
             var l1 = newLabel("Region size:");
             createSlider(s1, 1, 50, 1, [15], false, function() {processImage(originalImageSrc)});
@@ -23,6 +27,7 @@ $(document).ready (function() {
             var l4 = newLabel("Lower and upper bounds:");
             createSlider(s4, 0, 255, 1, [20, 150], true, function() {processImage(originalImageSrc)});
             node.append(l1, s1, l2, s2, l3, s3, l4, s4);
+            // show sliders and binarize image with default parameters
             node.slideDown("fast", function() {
                 processImage(originalImageSrc);
             });
@@ -49,6 +54,7 @@ $(document).ready (function() {
 });
 
 createSlider = function(mySliderDiv, myMin, myMax, myStep, myValues, myRange, myFunction) {
+    // creates a jQuery slider using parameters passed in
     mySliderDiv.slider({
         min: myMin,
         max: myMax,
